f501595741fe06b01bf1df2c115bb176e2e164eb
이동욱 <naomicylu@naver.com>
2023-07-25 09:18:44 +0900

    폰트추가

--- src/main.js
+++ src/main.js
@@ -2,23 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@/assets/css/common.css';
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-/* import specific icons */
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-
-/* add icons to the library */
-library.add(fas, far, fab);
-
-/* add font awesome icon component */
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(BootstrapVue, IconsPlugin);
 
 Vue.config.productionTip = false;
 
