8f0a2fdfde1874472cb89dd7d064ed4b54c72993
이동욱 <naomicylu@naver.com>
2023-08-16 13:25:35 +0900

    lodash

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import _ from 'lodash';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@/assets/css/common.css';
+import '@/assets/css/item.css';
 
 Vue.use(BootstrapVue, IconsPlugin);
 
